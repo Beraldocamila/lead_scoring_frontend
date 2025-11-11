af3e0c79b362a6b91e826dcb59811098d2c33395
angie <angelesmendozafarias@gmail.com>
2025-11-11 20:17:32 -0300

    version estable para integracion

--- src/pages/DetalleCliente/DetalleCliente.jsx
+++ src/pages/DetalleCliente/DetalleCliente.jsx
@@ -18,6 +18,7 @@ const ProfileItem = ({ icon: Icon, title, value }) => (
       <p className="profile-title">{title}</p>
       <p className="profile-value">{value}</p>
     </div>
+    
   </div>
 );
 
